946586acebaa180c8b5506794f0f96f6cab10ef2
MR.FIRST <128628624+Uncontested26@users.noreply.github.com>
2024-12-01 22:14:32 +0300

    new

---
+++ src/test/java/org/example/portalproject/PortalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.portalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
